0c211bb918b40128dfd159291d201e6660a0978a
marti <marti@VivoBook-de-Martin>
2020-05-26 18:09:14 +0200

    Prueba 7 acabada

--- GestionDeConcesionarias_Grupo1E/testIntegracion/datos/testMenuDatos.java
+++ GestionDeConcesionarias_Grupo1E/testIntegracion/datos/testMenuDatos.java
@@ -2,23 +2,218 @@ package datos;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+
+import codigoPrincipal.IGestionMenus;
+import gestionDatos.GestionDatosImpl;
+import gestionMenus.GestionMenusImpl;
+
 
 class testMenuDatos {
+	
+	String dia;
+	ArrayList<Plato> primeros=new ArrayList<>();
+	ArrayList<Plato> segundos=new ArrayList<>();
+	ArrayList<Plato> postres=new ArrayList<>();
+	ArrayList<Bebida> bebidas=new ArrayList<>();
 
 	@BeforeEach
 	void setUp() throws Exception {
+		
+		dia = "Lunes";
+		Plato plato = new Plato("primero", "Macarrones con queso");
+		primeros.add(plato);
+		plato = new Plato("primero", "Atún con queso");
+		primeros.add(plato);
+		plato = new Plato("primero", "Sopa fría");
+		primeros.add(plato);
+		plato = new Plato("segundo", "Pollo asado");
+		segundos.add(plato);
+		plato = new Plato("segundo", "Judías");
+		segundos.add(plato);
+		plato = new Plato("segundo", "Patatas fritas");
+		segundos.add(plato);
+		plato = new Plato("postre", "flan de huevo");
+		postres.add(plato);
+		plato = new Plato("postre", "helado de fresa");
+		postres.add(plato);
+		plato = new Plato("postre", "flan de café");
+		postres.add(plato);
+		Bebida bebida = new Bebida("agua");
+		bebidas.add(bebida);
+		bebida = new Bebida("Coca cola");
+		bebidas.add(bebida);
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
 	}
 
+	@DisplayName("CP21: Test integracion: se crea y se escribe un menu en el archivo.")
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testAlmacenarMenuConstruido() {
+		
+		JSONObject objMenus = null;
+		try {
+			String content = new String(Files.readAllBytes(Paths.get("./src/menu.json")));
+			objMenus = new JSONObject(content);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		JSONArray menusArr = objMenus.getJSONArray("menu");
+		int x = menusArr.length() - 1;
+		
+		GestionDatosImpl gD = new GestionDatosImpl();
+		GestionMenusImpl mG = new GestionMenusImpl(gD);
+		mG.construirMenu(dia, primeros, segundos, postres, bebidas);
+		
+		try {
+			String content = new String(Files.readAllBytes(Paths.get("./src/menu.json")));
+			objMenus = new JSONObject(content);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		menusArr = objMenus.getJSONArray("menu");
+		int y;
+		y = menusArr.length() - 1;
+		assertEquals(1, y - x); //Vemos si aumentó el contenido del fichero de menus
+	}
+	
+	@DisplayName("CP22: Test integracion: sin bebidas.")
+	@Test
+	void testAlmacenarMenuConstruidoBebidaNula() {
+		
+		JSONObject objMenus = null;
+		try {
+			String content = new String(Files.readAllBytes(Paths.get("./src/menu.json")));
+			objMenus = new JSONObject(content);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		JSONArray menusArr = objMenus.getJSONArray("menu");
+		int x = menusArr.length() - 1;
+		
+		GestionDatosImpl gD = new GestionDatosImpl();
+		GestionMenusImpl mG = new GestionMenusImpl(gD);
+		bebidas.clear();
+		mG.construirMenu(dia, primeros, segundos, postres, bebidas);
+		
+		try {
+			String content = new String(Files.readAllBytes(Paths.get("./src/menu.json")));
+			objMenus = new JSONObject(content);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		menusArr = objMenus.getJSONArray("menu");
+		int y;
+		y = menusArr.length() - 1;
+		assertEquals(1, y - x); //Vemos si aumentó el contenido del fichero de menus
+	}
+	
+	@DisplayName("CP23: Test integracion: dia no laborable.")
+	@Test
+	void testAlmacenarMenuConstruidoNoLaborable() {
+		
+		JSONObject objMenus = null;
+		try {
+			String content = new String(Files.readAllBytes(Paths.get("./src/menu.json")));
+			objMenus = new JSONObject(content);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		JSONArray menusArr = objMenus.getJSONArray("menu");
+		int x = menusArr.length() - 1;
+		
+		GestionDatosImpl gD = new GestionDatosImpl();
+		GestionMenusImpl mG = new GestionMenusImpl(gD);
+		dia = "Domingo";
+		mG.construirMenu(dia, primeros, segundos, postres, bebidas);
+		
+		try {
+			String content = new String(Files.readAllBytes(Paths.get("./src/menu.json")));
+			objMenus = new JSONObject(content);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		menusArr = objMenus.getJSONArray("menu");
+		int y;
+		y = menusArr.length() - 1;
+		assertEquals(1, y - x); //Vemos si aumentó el contenido del fichero de menus
+	}
+	
+	@DisplayName("CP24: Test integracion: con dos segundos solo.")
+	@Test
+	void testNoAlmacenarMenuConstruido() {
+		
+		JSONObject objMenus = null;
+		try {
+			String content = new String(Files.readAllBytes(Paths.get("./src/menu.json")));
+			objMenus = new JSONObject(content);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		JSONArray menusArr = objMenus.getJSONArray("menu");
+		int x = menusArr.length() - 1;
+		
+		GestionDatosImpl gD = new GestionDatosImpl();
+		GestionMenusImpl mG = new GestionMenusImpl(gD);
+		bebidas.clear();
+		segundos.remove(2);
+		mG.construirMenu(dia, primeros, segundos, postres, bebidas);
+		
+		try {
+			String content = new String(Files.readAllBytes(Paths.get("./src/menu.json")));
+			objMenus = new JSONObject(content);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		menusArr = objMenus.getJSONArray("menu");
+		int y;
+		y = menusArr.length() - 1;
+		assertEquals(1, y - x); //Vemos si aumentó el contenido del fichero de menus
+	}
+	
+	@DisplayName("CP25: Test integracion: con un dia inexistente.")
+	@Test
+	void testAlmacenarMenuConstruidoDiaErroneo() {
+		
+		JSONObject objMenus = null;
+		try {
+			String content = new String(Files.readAllBytes(Paths.get("./src/menu.json")));
+			objMenus = new JSONObject(content);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		JSONArray menusArr = objMenus.getJSONArray("menu");
+		int x = menusArr.length() - 1;
+		
+		GestionDatosImpl gD = new GestionDatosImpl();
+		GestionMenusImpl mG = new GestionMenusImpl(gD);
+		bebidas.remove(1);
+		bebidas.add(new Bebida("agua"));
+		dia = "Maandag";
+		mG.construirMenu(dia, primeros, segundos, postres, bebidas);
+		
+		try {
+			String content = new String(Files.readAllBytes(Paths.get("./src/menu.json")));
+			objMenus = new JSONObject(content);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		menusArr = objMenus.getJSONArray("menu");
+		int y;
+		y = menusArr.length() - 1;
+		assertEquals(1, y - x); //Vemos si aumentó el contenido del fichero de menus
 	}
 
 }
--- GestionDeConcesionarias_Grupo1E/testUnidad/gestionDatos/testGestionDatosImpl.java
+++ GestionDeConcesionarias_Grupo1E/testUnidad/gestionDatos/testGestionDatosImpl.java
@@ -1,7 +1,7 @@
 package gestionDatos;
+
 import datos.Menu;
 import static org.junit.jupiter.api.Assertions.*;
-
 
 import java.io.File;
 import java.nio.file.Files;
@@ -12,6 +12,7 @@ import org.json.JSONArray;
 import org.json.JSONObject;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import datos.Bebida;
@@ -19,72 +20,69 @@ import datos.Plato;
 
 class testGestionDatosImpl {
 	String dia;
-	ArrayList<Plato> primeros=new ArrayList<>();
-	ArrayList<Plato> segundos=new ArrayList<>();
-	ArrayList<Plato> postres=new ArrayList<>();
-	ArrayList<Bebida> bebidas=new ArrayList<>();
+	ArrayList<Plato> primeros = new ArrayList<>();
+	ArrayList<Plato> segundos = new ArrayList<>();
+	ArrayList<Plato> postres = new ArrayList<>();
+	ArrayList<Bebida> bebidas = new ArrayList<>();
 
 	@BeforeEach
 	void setUp() throws Exception {
-		dia="Lunes";
-		Plato plato=new Plato("primero","Macarrones con queso");
+		dia = "Lunes";
+		Plato plato = new Plato("primero", "Macarrones con queso");
 		primeros.add(plato);
-		plato=new Plato("primero","Atún con queso");
+		plato = new Plato("primero", "Atún con queso");
 		primeros.add(plato);
-		plato=new Plato("primero","Sopa fría");
+		plato = new Plato("primero", "Sopa fría");
 		primeros.add(plato);
-		plato=new Plato("segundo","Pollo asado");
+		plato = new Plato("segundo", "Pollo asado");
 		segundos.add(plato);
-		plato=new Plato("segundo","Judías");
+		plato = new Plato("segundo", "Judías");
 		segundos.add(plato);
-		plato=new Plato("segundo","Patatas fritas");
+		plato = new Plato("segundo", "Patatas fritas");
 		segundos.add(plato);
-		plato=new Plato("postre","flan de huevo");
+		plato = new Plato("postre", "flan de huevo");
 		postres.add(plato);
-		plato=new Plato("postre","helado de fresa");
+		plato = new Plato("postre", "helado de fresa");
 		postres.add(plato);
-		plato=new Plato("postre","flan de café");
+		plato = new Plato("postre", "flan de café");
 		postres.add(plato);
-		Bebida bebida=new Bebida("agua");
+		Bebida bebida = new Bebida("agua");
 		bebidas.add(bebida);
-		bebida=new Bebida("Coca cola");
+		bebida = new Bebida("Coca cola");
 		bebidas.add(bebida);
-		
-		
-		
-		
+
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
-	
+
 	}
 
+	@DisplayName("CP06: Test almacenarMenu: se escribe un menu en el archivo.")
 	@Test
 	void testAlmacenarMenuCorrecto() {
-		JSONObject objMenus = null,objPlatos = null;
+		JSONObject objMenus = null, objPlatos = null;
 		try {
 			String content = new String(Files.readAllBytes(Paths.get("./src/menu.json")));
 			objMenus = new JSONObject(content);
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-			JSONArray menusArr = objMenus.getJSONArray("menu");
-			int x=menusArr.length()-1;
-			int y;
-		Menu menu=new Menu(primeros,segundos,postres,bebidas,dia);
-		GestionDatosImpl gD=new GestionDatosImpl();
+		JSONArray menusArr = objMenus.getJSONArray("menu");
+		int x = menusArr.length() - 1;
+		int y;
+		Menu menu = new Menu(primeros, segundos, postres, bebidas, dia);
+		GestionDatosImpl gD = new GestionDatosImpl();
 		gD.almacenarMenu(menu);
 		try {
 			String content = new String(Files.readAllBytes(Paths.get("./src/menu.json")));
 			objMenus = new JSONObject(content);
-		}catch(Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		 menusArr = objMenus.getJSONArray("menu");
-		 y=menusArr.length()-1;
-		assertEquals(1, y-x);
-				
+		menusArr = objMenus.getJSONArray("menu");
+		y = menusArr.length() - 1;
+		assertEquals(1, y - x);
 	}
 
 }
