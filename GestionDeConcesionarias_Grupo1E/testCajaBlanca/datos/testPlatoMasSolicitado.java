b257c42aeb40e9b2f077bbbb113c8434cd3b82ec
maria <maria@localhost>
2020-05-26 11:26:59 +0200

    testSeleccionMenusImpl y borrador de platoMasSolicitado

---
+++ GestionDeConcesionarias_Grupo1E/testCajaBlanca/datos/testPlatoMasSolicitado.java
@@ -0,0 +1,101 @@
+package datos;
+
+import static org.junit.Assert.assertNull;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.HashMap;
+
+import org.junit.Assert;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Tag;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+
+import estadisticas.EstadisticasImpl;
+import sensores.SensoresImpl;
+
+public class testPlatoMasSolicitado {
+	
+	HashMap<Plato,Integer> platos;
+	Plato plato;
+	SensoresImpl sensores;
+	@InjectMocks
+	EstadisticasImpl est;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		platos=new HashMap<>();
+		plato=new Plato(" "," ");
+		sensores=new SensoresImpl();
+		est=new EstadisticasImpl(sensores);
+		
+	}
+	
+	//Prueba de caja blanca
+	@DisplayName("P11_CP35")
+	@Tag("PlatoMasSolicitado")
+	@Tag("CajaBlanca")
+	@Test
+	
+	void testSolicitadoCP35() {
+		//caso de hashmap nulo
+		//el plato tiene que ser nulo
+		assertNull(est.platoMasSolicitado(null), "El plato devuelto debe ser nulo.");
+	}
+	
+	@DisplayName("P11_CP36")
+	@Tag("PlatoMasSolicitado")
+	@Tag("CajaBlanca")
+	@Test
+	
+	void testSolicitadoCP36() {
+		//caso de integer 0
+		plato=new Plato("postre","helado de fresa");
+		platos.put(plato,0);
+		
+		
+		//debe devolver un plato distinto
+		assertNotEquals(plato.getNombre(),est.platoMasSolicitado(platos).getNombre(),"El plato devuelto debe distinto.");
+		assertNotEquals(plato.getTipo(),est.platoMasSolicitado(platos).getTipo(),"El plato devuelto debe ser distinto.");
+
+	}
+	
+	@DisplayName("P11_CP37")
+	@Tag("PlatoMasSolicitado")
+	@Tag("CajaBlanca")
+	@Test
+	
+	void testSolicitadoCP37() {
+		//caso de 1 valoracion
+		plato=new Plato("postre","helado de fresa");
+		platos.put(plato,6);
+		
+		
+		//debe devolver el propio plato
+		assertEquals(plato.getNombre(),est.platoMasSolicitado(platos).getNombre(),"El plato devuelto debe ser el mismo.");
+		assertEquals(plato.getTipo(),est.platoMasSolicitado(platos).getTipo(),"El plato devuelto debe ser el mismo.");
+	}
+	
+	@DisplayName("P11_CP38")
+	@Tag("PlatoMasSolicitado")
+	@Tag("CajaBlanca")
+	@Test
+	
+	void testSolicitadoCP38() {
+		//caso de 2 valoraciones
+		plato=new Plato("postre","helado de fresa");
+		platos.put(plato,10);
+		plato=new Plato("postre","Flan de café");
+		platos.put(plato,7);
+		
+		//debe devolver el primer plato del array
+		assertEquals(plato.getNombre(),est.platoMasSolicitado(platos).getNombre(),"El plato devuelto debe ser el primero del array.");
+		assertEquals(plato.getTipo(),est.platoMasSolicitado(platos).getTipo(),"El plato devuelto debe ser el primero del array.");
+	}
+
+}
--- GestionDeConcesionarias_Grupo1E/testUnidad/seleccionMenus/testSeleccionMenusImpl.java
+++ GestionDeConcesionarias_Grupo1E/testUnidad/seleccionMenus/testSeleccionMenusImpl.java
@@ -1,24 +1,340 @@
 package seleccionMenus;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
+import java.awt.Menu;
+import java.util.ArrayList;
+
+import datos.Bebida;
+import datos.Plato;
+import gestionDatos.GestionDatosImpl;
+import gestionMenus.GestionMenusImpl;
+import codigoPrincipal.ISeleccionMenus;
+import seleccionMenus.SeleccionMenusImpl;
+import sensores.SensoresImpl;
 
 class testSeleccionMenusImpl {
 
+	Plato primero;
+	Plato segundo;
+	Plato postre;
+	Bebida bebida;
+	GestionDatosImpl gD;
+	GestionMenusImpl mG;
+	@InjectMocks
+	SeleccionMenusImpl sM;
+
 	@BeforeEach
-	void setUp() throws Exception {
+	void setUp() throws Exception{
+		sM=Mockito.mock(SeleccionMenusImpl.class);
+		gD=new GestionDatosImpl();
+		mG=new GestionMenusImpl(gD);
+		
+		// Inyectamos en las clases anotadas sus clases simuladas
+		MockitoAnnotations.initMocks(this);
+		Plato primero=new Plato(" ", " ");
+		Plato segundo=new Plato(" ", " ");
+		Plato postre=new Plato(" ", " ");
+		Bebida bebida=new Bebida(" ");
+		
 	}
 
-	@AfterEach
-	void tearDown() throws Exception {
-	}
-
+	@DisplayName("CP08: Test seleccionarMenu: todos los platos son del menu del dia.")
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testSeleccionarMenuCorrecto() {
+		
+		datos.Menu menu=mG.mostrarMenuDelDia();
+		
+		Plato primero=new Plato("primero", "Sopa fría");
+		Plato segundo=new Plato("segundo", "Pollo asado");
+		Plato postre=new Plato("postre", "helado de fresa");
+		Bebida bebida=new Bebida("agua");
+		
+		assertEquals("primero",primero.getTipo(), "El plato introducido como primero no es un primero.");
+		assertEquals("segundo",primero.getTipo(), "El plato introducido como segundo no es un segundo.");
+		assertEquals("postre",primero.getTipo(), "El plato introducido  como postre no es un postre.");
+	
+		
+		for(Plato prim:menu.getPrimerosDisponibles()) {
+			
+			if(prim.getNombre().equals(primero.getNombre())) {
+				assertEquals(prim.getNombre(),primero.getNombre(),"El primero introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Plato seg:menu.getSegundosDisponibles()) {
+			
+			if(seg.getNombre().equals(segundo.getNombre())) {
+				assertEquals(seg.getNombre(),segundo.getNombre(),"El segundo introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Plato pos:menu.getPostresDisponibles()) {
+			
+			if(pos.getNombre().equals(postre.getNombre())) {
+				assertEquals(pos.getNombre(),postre.getNombre(),"El postre introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Bebida beb:menu.getBebidas()) {
+			
+			if(beb.getNombre().equals(bebida.getNombre())) {
+				assertEquals(beb.getNombre(),bebida.getNombre(),"La bebida introducida no esta en la lista.");
+			}
+		}
+		
+		sM.seleccionarMenu(primero, segundo, postre, bebida);
+		
+		
+	}
+	
+	@DisplayName("CP09: Test seleccionarMenu: el primero introducido no es del tipo primero.")
+	@Test
+	void testSeleccionarMenuPrimeroIncorrecto() {
+		
+		datos.Menu menu=mG.mostrarMenuDelDia();
+		
+		Plato primero=new Plato("primero", "Patatas fritas");
+		Plato segundo=new Plato("segundo", "Pollo asado");
+		Plato postre=new Plato("postre", "helado de fresa");
+		Bebida bebida=new Bebida("agua");
+		
+		assertEquals("primero",primero.getTipo(), "El plato introducido como primero no es un primero.");
+		assertEquals("segundo",primero.getTipo(), "El plato introducido como segundo no es un segundo.");
+		assertEquals("postre",primero.getTipo(), "El plato introducido  como postre no es un postre.");
+	
+		
+		for(Plato prim:menu.getPrimerosDisponibles()) {
+			
+			if(prim.getNombre().equals(primero.getNombre())) {
+				assertEquals(prim.getNombre(),primero.getNombre(),"El primero introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Plato seg:menu.getSegundosDisponibles()) {
+			
+			if(seg.getNombre().equals(segundo.getNombre())) {
+				assertEquals(seg.getNombre(),segundo.getNombre(),"El segundo introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Plato pos:menu.getPostresDisponibles()) {
+			
+			if(pos.getNombre().equals(postre.getNombre())) {
+				assertEquals(pos.getNombre(),postre.getNombre(),"El postre introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Bebida beb:menu.getBebidas()) {
+			
+			if(beb.getNombre().equals(bebida.getNombre())) {
+				assertEquals(beb.getNombre(),bebida.getNombre(),"La bebida introducida no esta en la lista.");
+			}
+		}
+		
+		sM.seleccionarMenu(primero, segundo, postre, bebida);
+		
+		
+	}
+	
+	@DisplayName("CP10: Test seleccionarMenu: el segundo introducido no es del tipo segundo.")
+	@Test
+	void testSeleccionarMenuSegundoIncorrecto() {
+		
+		datos.Menu menu=mG.mostrarMenuDelDia();
+		
+		Plato primero=new Plato("primero", "Sopa fría");
+		Plato segundo=new Plato("segundo", "Atún con queso");
+		Plato postre=new Plato("postre", "helado de fresa");
+		Bebida bebida=new Bebida("agua");
+		
+		assertEquals("primero",primero.getTipo(), "El plato introducido como primero no es un primero.");
+		assertEquals("segundo",primero.getTipo(), "El plato introducido como segundo no es un segundo.");
+		assertEquals("postre",primero.getTipo(), "El plato introducido  como postre no es un postre.");
+	
+		
+		for(Plato prim:menu.getPrimerosDisponibles()) {
+			
+			if(prim.getNombre().equals(primero.getNombre())) {
+				assertEquals(prim.getNombre(),primero.getNombre(),"El primero introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Plato seg:menu.getSegundosDisponibles()) {
+			
+			if(seg.getNombre().equals(segundo.getNombre())) {
+				assertEquals(seg.getNombre(),segundo.getNombre(),"El segundo introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Plato pos:menu.getPostresDisponibles()) {
+			
+			if(pos.getNombre().equals(postre.getNombre())) {
+				assertEquals(pos.getNombre(),postre.getNombre(),"El postre introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Bebida beb:menu.getBebidas()) {
+			
+			if(beb.getNombre().equals(bebida.getNombre())) {
+				assertEquals(beb.getNombre(),bebida.getNombre(),"La bebida introducida no esta en la lista.");
+			}
+		}
+		
+		sM.seleccionarMenu(primero, segundo, postre, bebida);
+		
+		
+	}
+	
+	@DisplayName("CP11: Test seleccionarMenu: el postre introducido no es del tipo postre.")
+	@Test
+	void testSeleccionarMenuPostreIncorrecto() {
+		
+		datos.Menu menu=mG.mostrarMenuDelDia();
+		
+		Plato primero=new Plato("primero", "Sopa fría");
+		Plato segundo=new Plato("segundo", "Pollo asado");
+		Plato postre=new Plato("postre", "Pollo asado");
+		Bebida bebida=new Bebida("agua");
+		
+		assertEquals("primero",primero.getTipo(), "El plato introducido como primero no es un primero.");
+		assertEquals("segundo",primero.getTipo(), "El plato introducido como segundo no es un segundo.");
+		assertEquals("postre",primero.getTipo(), "El plato introducido  como postre no es un postre.");
+	
+		
+		for(Plato prim:menu.getPrimerosDisponibles()) {
+			
+			if(prim.getNombre().equals(primero.getNombre())) {
+				assertEquals(prim.getNombre(),primero.getNombre(),"El primero introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Plato seg:menu.getSegundosDisponibles()) {
+			
+			if(seg.getNombre().equals(segundo.getNombre())) {
+				assertEquals(seg.getNombre(),segundo.getNombre(),"El segundo introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Plato pos:menu.getPostresDisponibles()) {
+			
+			if(pos.getNombre().equals(postre.getNombre())) {
+				assertEquals(pos.getNombre(),postre.getNombre(),"El postre introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Bebida beb:menu.getBebidas()) {
+			
+			if(beb.getNombre().equals(bebida.getNombre())) {
+				assertEquals(beb.getNombre(),bebida.getNombre(),"La bebida introducida no esta en la lista.");
+			}
+		}
+		
+		sM.seleccionarMenu(primero, segundo, postre, bebida);
+		
+		
+	}
+	
+	@DisplayName("CP12: Test seleccionarMenu: la bebida no es válida.")
+	@Test
+	void testSeleccionarMenuBebidaIncorrecta() {
+		
+		datos.Menu menu=mG.mostrarMenuDelDia();
+		
+		Plato primero=new Plato("primero", "Sopa fría");
+		Plato segundo=new Plato("segundo", "Pollo asado");
+		Plato postre=new Plato("postre", "helado de fresa");
+		
+		assertEquals("primero",primero.getTipo(), "El plato introducido como primero no es un primero.");
+		assertEquals("segundo",primero.getTipo(), "El plato introducido como segundo no es un segundo.");
+		assertEquals("postre",primero.getTipo(), "El plato introducido  como postre no es un postre.");
+	
+		
+		for(Plato prim:menu.getPrimerosDisponibles()) {
+			
+			if(prim.getNombre().equals(primero.getNombre())) {
+				assertEquals(prim.getNombre(),primero.getNombre(),"El primero introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Plato seg:menu.getSegundosDisponibles()) {
+			
+			if(seg.getNombre().equals(segundo.getNombre())) {
+				assertEquals(seg.getNombre(),segundo.getNombre(),"El segundo introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Plato pos:menu.getPostresDisponibles()) {
+			
+			if(pos.getNombre().equals(postre.getNombre())) {
+				assertEquals(pos.getNombre(),postre.getNombre(),"El postre introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Bebida beb:menu.getBebidas()) {
+			
+			if(beb.getNombre().equals(bebida.getNombre())) {
+				assertEquals(beb.getNombre(),bebida.getNombre(),"La bebida introducida no esta en la lista.");
+			}
+		}
+		
+		sM.seleccionarMenu(primero, segundo, postre, bebida);
+		
+		
+	}
+	
+	@DisplayName("CP13: Test seleccionarMenu: platos del tipo correcto pero nombre inexistente.")
+	@Test
+	void testSeleccionarMenuDatosNoExistentes() {
+		
+		datos.Menu menu=mG.mostrarMenuDelDia();
+		
+		Plato primero=new Plato("primero", "kefjwf");
+		Plato segundo=new Plato("segundo", "ewrgferg");
+		Plato postre=new Plato("postre", "fewgrreg");
+		
+		assertEquals("primero",primero.getTipo(), "El plato introducido como primero no es un primero.");
+		assertEquals("segundo",primero.getTipo(), "El plato introducido como segundo no es un segundo.");
+		assertEquals("postre",primero.getTipo(), "El plato introducido  como postre no es un postre.");
+	
+		
+		for(Plato prim:menu.getPrimerosDisponibles()) {
+			
+			if(prim.getNombre().equals(primero.getNombre())) {
+				assertEquals(prim.getNombre(),primero.getNombre(),"El primero introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Plato seg:menu.getSegundosDisponibles()) {
+			
+			if(seg.getNombre().equals(segundo.getNombre())) {
+				assertEquals(seg.getNombre(),segundo.getNombre(),"El segundo introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Plato pos:menu.getPostresDisponibles()) {
+			
+			if(pos.getNombre().equals(postre.getNombre())) {
+				assertEquals(pos.getNombre(),postre.getNombre(),"El postre introducido no esta en el menu del dia.");
+			}
+		}
+		
+		for(Bebida beb:menu.getBebidas()) {
+			
+			if(beb.getNombre().equals(bebida.getNombre())) {
+				assertEquals(beb.getNombre(),bebida.getNombre(),"La bebida introducida no esta en la lista.");
+			}
+		}
+		
+		sM.seleccionarMenu(primero, segundo, postre, bebida);
+		
+		
 	}
 
 }
